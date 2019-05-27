cc9d7f1051bff17471ae3d4d3f1f407a41ad1731
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2019-05-27 19:48:02 +0300

    *Lowered minimum required nodejs version, * Success handling is separated * Auto focus to textarea after Result fadeIn

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,18 +10,28 @@
 
 const searchForm = $('#searchform');
 const result = $('.search-result');
+const resultContainer = $('#result-textarea');
 
 const handleSuccess = (data) => {
     const { songs } = data; // const songs = data.songs;
 
     $('#result-textarea').val($.map(songs, link => 'spotify:track:' + link).join('\n'));
-    $('#result-container').fadeIn('fast');
+    $('#result-container').fadeIn('fast', () => {
+        $('#result-textarea').focus();
+        $('#result-textarea').select();
+    });
+};
+
+const handleError = () => {
+    $('#error-message').html('<p>Error fetching your data. This may be due wrong username or api connection error.</p><p>Please refresh the page and try again</p>').show();
+    $('#message-container').fadeIn('fast');
 };
 
 const hideContainers = () => {
     $('div[rel="alert"], div[rel="container"]').hide();
 };
 
+// Events
 searchForm.submit((e) => {
     e.preventDefault();
     hideContainers();
@@ -31,16 +41,15 @@ searchForm.submit((e) => {
         dataType: 'json',
         data: searchForm.serialize(),
         success: handleSuccess,
-        error: () => {
-            $('#error-message').html('<p>Error fetching your data. This may be due wrong username or api connection error.</p><p>Please refresh the page and try again</p>').show();
-            $('#message-container').fadeIn('fast');
-        },
+        error: handleError,
     });
 });
 
-result.click(() => {
+result.on('click', () => {
     result.select();
 });
+
+resultContainer.on('select focus', () => document.execCommand('copy'));
 
 // Enable the popover tooltips:
 $('[data-toggle="popover"]').popover();
